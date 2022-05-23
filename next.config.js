cb65b96c8c93266d339be434d6bd248a4dc19ae4
Surjith S M <surjithctly@gmail.com>
2022-05-23 18:52:40 +0530

    fix build & studio setup

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
-const STUDIO_REWRITE = {
-  source: "/studio/:path*",
-  destination:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3333/studio/:path*"
-      : "/studio/index.html"
-};
-
 module.exports = {
   images: {
     domains: ["cdn.sanity.io"],
     loader: "custom"
   }
-  // rewrites: () => [STUDIO_REWRITE],
 };
