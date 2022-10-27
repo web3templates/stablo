9a7e07cdc00dbeae429c213aea8eb4b687929144
Surjith S M <surjithctly@gmail.com>
2022-10-27 12:03:45 +0530

    fix broken category link

--- components/blog/category.js
+++ components/blog/category.js
@@ -6,9 +6,7 @@ export default function CategoryLabel({ categories }) {
     <div className="flex gap-3">
       {categories?.length &&
         categories.slice(0).map((category, index) => (
-          <Link
-            href={`/category/${category.slug.current}`}
-            key={index}>
+          <Link href="#" key={index}>
             <a>
               <Label color={category.color}>{category.title}</Label>
             </a>
