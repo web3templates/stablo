facd21c9fd503180578993a38577973d26e12f87
Surjith S M <surjithctly@gmail.com>
2023-07-31 11:20:52 +0530

    update next

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true
-  },
   images: {
     formats: ["image/avif", "image/webp"],
     dangerouslyAllowSVG: true,
