057ca587aa1e059738289fce746497527fb3a0ea
gollbik <golbanalexandru2005@gmail.com>
2024-06-17 22:57:08 +0300

    Maind page changes

--- components/container.js
+++ components/container.js
@@ -4,8 +4,7 @@ export default function Container(props) {
   return (
     <div
       className={cx(
-        "container px-8 mx-auto xl:px-5",
-        props.large ? " max-w-screen-xl" : " max-w-screen-lg",
+        "container mx-auto px-8 xl:px-5",
         !props.alt && "py-5 lg:py-8",
         props.className
       )}>
